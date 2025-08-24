c44ddb9aca728cc6d6f50d5523dda90ce5e42fd4
TomaShops Developer <your-email@example.com>
2025-08-24 14:09:44 -0400

    Update third video on How It Works page with new YouTube link

--- src/pages/HowItWorks.tsx
+++ src/pages/HowItWorks.tsx
@@ -90,7 +90,7 @@ const HowItWorks: React.FC = () => {
         <div className="relative w-full" style={{ paddingBottom: '56.25%' }}>
           <iframe
             className="absolute top-0 left-0 w-full h-full rounded-lg shadow-lg"
-            src="https://www.youtube.com/embed/uS_JhdsZpcg"
+            src="https://www.youtube.com/embed/5WDg6qm7Crc"
             title="TomaShops Additional Demo"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
